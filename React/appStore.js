06c442033af22dce35811a41d021fbb9d6743c25
shubhamkarad <shubhamkaradsdk07@gmail.com>
2024-12-14 23:11:44 +0530

    Redux store in React

--- React/appStore.js
+++ React/appStore.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import cartReducer from "./cartSlice";
+import cartReducer, { addItem } from "./cartSlice";
 
 const appStore = configureStore({
   reducer: {
@@ -8,3 +8,17 @@ const appStore = configureStore({
 });
 
 export default appStore;
+
+// Selector is a hook in react
+// That we can use in the react component and that is coming from the React Redux library.
+
+// Subscribing to the selector,
+const cart = useSelector((store) => store.cart, items);
+
+// useDispatch - To dipatch the action.  {Eg: Add item to the cart}
+
+const dipatch = useDispatch();
+
+const handleItem = (productId) => {
+  dipatch(addItem(productId));
+};
